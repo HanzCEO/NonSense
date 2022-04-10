ffd21f1f38a6b36d759e9387df53056ff5b40e8b
HanzCEO <hanzhaxors@gmail.com>
2022-04-10 19:25:14 +0700

    feat: Basic functionality

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from random import choice
+from time import sleep
 
 a = "aiueo"
 b = "bcdfghjklmnpqrstvwxyz"
+length = int(input("Length (5): ") or 5)
+auto = bool(input("Auto drive (false): "))
 
 def suggest(l):
 	f = "a"
 	if choice([1,0]):
 		f += choice(a)
 
-	for i in range(l-1):
+	for i in range(l):
 		if f[-1] in a:
 			f += choice(b)
 		else:
 			f += choice(a)
-	return f[1:]
+	return f[1:l+1]
 
 print("Enter/Return - Get more suggestion")
-print("q/ctrl+c - Quit")
+
+if not auto:
+	print("q/ctrl+c - Quit")
+else:
+	print("ctrl+c - Quit")
+
 while True:
-	print(suggest(6), end="")
+	suggestion = suggest(length)
 
 	try:
-		inp = input()
-		if inp == "q": break
+		if not auto:
+			print(suggestion, end="")
+			inp = input()
+			if inp == "q":
+				break
+		else:
+			print(suggestion)
+			sleep(1)
 	except KeyboardInterrupt:
 		break
